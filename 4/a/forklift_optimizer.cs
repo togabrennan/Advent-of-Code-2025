2d74bca578035404f853f36e85736fd27f657f14
Brennan Hitchcock <brennan.hitchcock@gmail.com>
2025-12-04 19:16:57 -0500

    Day 4 solution

---
+++ 4/a/forklift_optimizer.cs
@@ -0,0 +1,69 @@
+var lines = File.ReadAllLines("input.txt");
+const int ACCESSIBLE_LIMIT = 4;
+
+int accessibleRolls = 0;
+char[,] floor = BuildFloorArray(lines);
+char[,] printFloor = BuildFloorArray(lines);
+
+for (int i = 0; i < floor.GetLength(0); i++)
+{
+    for (int j = 0; j < floor.GetLength(1); j++)
+    {
+
+        if (floor[i, j] == '@') 
+        {
+            int rollsAround = CalcRollsAroundLocation(floor, (i, j));
+            if (rollsAround < ACCESSIBLE_LIMIT)
+            {
+                printFloor[i, j] = 'x';
+                accessibleRolls++;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"The number of accessible forklift rolls is: {accessibleRolls}");
+
+int CalcRollsAroundLocation(char[,] floor, (int, int) loc)
+{
+    int rollsAround = 0;
+    var directions = new (int, int)[]
+    {
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),  /*loc*/  (0, 1),   
+        (1, -1),  (1, 0),  (1, 1)
+    };
+
+    foreach (var (dx, dy) in directions)
+    {
+        int newX = loc.Item1 + dx;
+        int newY = loc.Item2 + dy;
+
+        // Check bounds
+        if (newX >= 0 && newX < floor.GetLength(0) && newY >= 0 && newY < floor.GetLength(1))
+        {
+            if (floor[newX, newY] == '@')
+            {
+                rollsAround++;
+            }
+        }
+    }
+
+    return rollsAround;
+}
+
+char[,] BuildFloorArray(string[] lines)
+{
+    int rows = lines.Length;
+    int cols = lines[0].Length;
+    char[,] floor = new char[rows, cols];
+    for (int i = 0; i < lines.Length; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            floor[i, j] = lines[i][j];
+        }   
+    }
+
+    return floor;
+}
